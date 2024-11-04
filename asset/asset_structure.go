a2864283649841bd4538647b9b237deffb61506a
Zdenek Devaty <zdenek.devaty@eliona.io>
2024-11-04 16:52:56 +0100

    Migrate asset insertion to bulk import

--- asset/asset_structure.go
+++ asset/asset_structure.go
@@ -1,3 +1,18 @@
+//  This file is part of the eliona project.
+//  Copyright © 2024 LEICOM iTEC AG. All Rights Reserved.
+//  ______ _ _
+// |  ____| (_)
+// | |__  | |_  ___  _ __   __ _
+// |  __| | | |/ _ \| '_ \ / _` |
+// | |____| | | (_) | | | | (_| |
+// |______|_|_|\___/|_| |_|\__,_|
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 package asset
 
 import (
@@ -5,9 +20,6 @@ import (
 	"time"
 
 	api "github.com/eliona-smart-building-assistant/go-eliona-api-client/v2"
-	"github.com/eliona-smart-building-assistant/go-eliona-api-client/v2/tools"
-	"github.com/eliona-smart-building-assistant/go-eliona/client"
-	"github.com/eliona-smart-building-assistant/go-utils/common"
 )
 
 type Root interface {
@@ -35,213 +47,183 @@ type Asset interface {
 	SetAssetID(assetID int32, projectID string) error
 }
 
+// CreateAssets creates assets within the asset structure using bulk creation.
+// It skips assets that have assetID != nil but keeps their children.
+// NOTE: Prefer using CreateAssetsBulk method, avoiding complexity and too much
+// juggling of asset structure.
 func CreateAssets(root Root, projectId string) (createdCnt int, err error) {
-	return createAssetsAndUpsertData(root, projectId, false, nil, nil)
+	var assetsToCreate []AssetWithParentReferences
+
+	err = collectAssetsToCreate(root, "", "", &assetsToCreate, projectId)
+	if err != nil {
+		return 0, fmt.Errorf("collecting assets to create: %v", err)
+	}
+
+	createdCnt, err = createAssets(assetsToCreate, projectId)
+	if err != nil {
+		return 0, fmt.Errorf("creating assets: %v", err)
+	}
+
+	return createdCnt, nil
 }
 
+// CreateAssetsAndUpsertData creates assets within the asset structure and upserts their data.
+// It skips assets that have assetID != nil but keeps their children.
+// It uses the createAssets() function to bulk create assets.
+// NOTE: Prefer using CreateAssetsBulk method, avoiding complexity and too much
+// juggling of asset structure.
 func CreateAssetsAndUpsertData(root Root, projectId string, ts *time.Time, clientReference *string) (createdCnt int, err error) {
-	return createAssetsAndUpsertData(root, projectId, true, ts, clientReference)
-}
+	var assetsToCreate []AssetWithParentReferences
+	var dataToUpsert []Data
 
-func createAssetsAndUpsertData(root Root, projectId string, upsertingData bool, ts *time.Time, clientReference *string) (createdCnt int, err error) {
-	rootAssetID, created, err := createRoot(root, projectId)
+	err = collectAssetsAndDataToCreate(root, "", "", &assetsToCreate, &dataToUpsert, projectId, ts, clientReference)
 	if err != nil {
-		return createdCnt, fmt.Errorf("upserting root asset: %v", err)
-	}
-	if created {
-		createdCnt++
-	}
-	for _, fc := range root.GetFunctionalChildren() {
-		if fc == nil {
-			continue
-		}
-		traverseCreated, err := traverseFunctionalTree(fc, projectId, rootAssetID, rootAssetID, upsertingData, ts, clientReference)
-		if err != nil {
-			return createdCnt, fmt.Errorf("functional tree traversal: %v", err)
-		}
-		createdCnt += traverseCreated
+		return 0, fmt.Errorf("collecting assets and data to create: %v", err)
 	}
 
-	for _, lc := range root.GetLocationalChildren() {
-		if lc == nil {
-			continue
-		}
-		traverseCreated, err := traverseLocationalTree(lc, projectId, rootAssetID, rootAssetID, upsertingData, ts, clientReference)
-		if err != nil {
-			return createdCnt, fmt.Errorf("locational tree traversal: %v", err)
-		}
-		createdCnt += traverseCreated
+	createdCnt, err = createAssets(assetsToCreate, projectId)
+	if err != nil {
+		return 0, fmt.Errorf("creating assets: %v", err)
 	}
+
+	// Upsert data for all assets (including those that already existed)
+	for _, data := range dataToUpsert {
+		err := UpsertAssetDataIfAssetExists(data)
+		if err != nil {
+			return createdCnt, fmt.Errorf("upserting data: %v", err)
+		}
+	}
+
 	return createdCnt, nil
 }
 
-func traverseLocationalTree(
-	node LocationalNode,
-	projectId string,
-	locationalParentAssetId,
-	functionalParentAssetId *int32,
-	upsertingData bool,
-	ts *time.Time,
-	clientReference *string,
-) (createdCnt int, err error) {
-
-	currentAssetId, created, err := createAsset(node, projectId, locationalParentAssetId, functionalParentAssetId)
+// collectAssetsToCreate traverses the asset tree and collects assets that need to be created.
+func collectAssetsToCreate(node Asset, locationalParentGAI, functionalParentGAI string, assets *[]AssetWithParentReferences, projectId string) error {
+	assetID, err := node.GetAssetID(projectId)
 	if err != nil {
-		return createdCnt, err
-	}
-	if !created {
-		if err := setAssetParent(currentAssetId, node, projectId, locationalParentAssetId, nil); err != nil {
-			return createdCnt, fmt.Errorf("updating asset parent: %v", err)
-		}
-	}
-	if created {
-		createdCnt++
+		return fmt.Errorf("getting asset ID: %v", err)
 	}
 
-	if currentAssetId != nil && upsertingData {
-		err = upsertNodeDataIfAssetExists(node, *currentAssetId, ts, clientReference)
-		if err != nil {
-			return createdCnt, err
-		}
-	}
-
-	for _, child := range node.GetLocationalChildren() {
-		if child == nil {
-			continue
-		}
-		traverseCreated, err := traverseLocationalTree(child, projectId, currentAssetId, functionalParentAssetId, upsertingData, ts, clientReference)
-		if err != nil {
-			return createdCnt, err
-		}
-		createdCnt += traverseCreated
-	}
-	return createdCnt, nil
-}
-
-func traverseFunctionalTree(
-	node FunctionalNode,
-	projectId string,
-	locationalParentAssetId,
-	functionalParentAssetId *int32,
-	upsertingData bool,
-	ts *time.Time,
-	clientReference *string,
-) (createdCnt int, err error) {
-
-	currentAssetId, created, err := createAsset(node, projectId, locationalParentAssetId, functionalParentAssetId)
-	if err != nil {
-		return createdCnt, err
-	}
-	if !created {
-		if err := setAssetParent(currentAssetId, node, projectId, nil, functionalParentAssetId); err != nil {
-			return createdCnt, fmt.Errorf("updating asset parent: %v", err)
-		}
-	}
-	if created {
-		createdCnt++
-	}
-
-	if currentAssetId != nil && upsertingData {
-		err = upsertNodeDataIfAssetExists(node, *currentAssetId, ts, clientReference)
-		if err != nil {
-			return createdCnt, err
-		}
-	}
-
-	for _, child := range node.GetFunctionalChildren() {
-		if child == nil {
-			continue
-		}
-		traverseCreated, err := traverseFunctionalTree(child, projectId, locationalParentAssetId, currentAssetId, upsertingData, ts, clientReference)
-		if err != nil {
-			return createdCnt, err
-		}
-		createdCnt += traverseCreated
-	}
-	return createdCnt, nil
-}
-
-func upsertNodeDataIfAssetExists(node Asset, assetId int32, ts *time.Time, clientReference *string) error {
-	cr := ""
-	if clientReference != nil {
-		cr = *clientReference
-	}
-	return UpsertAssetDataIfAssetExists(Data{
-		AssetId:         assetId,
-		Timestamp:       *api.NewNullableTime(ts),
-		ClientReference: cr,
-		Data:            node,
-	})
-}
-
-func createRoot(ast Asset, projectId string) (assetId *int32, created bool, err error) {
-	return createAsset(ast, projectId, nil, nil)
-}
-
-func createAsset(ast Asset, projectId string, locationalParentAssetId *int32, functionalParentAssetId *int32) (assetId *int32, created bool, err error) {
-	originalAssetID, err := ast.GetAssetID(projectId)
-	if err != nil {
-		return nil, created, fmt.Errorf("getting asset id: %v", err)
-	}
-	if originalAssetID != nil {
-		return originalAssetID, created, nil
-	}
-	a := api.Asset{
-		ProjectId:               projectId,
-		GlobalAssetIdentifier:   ast.GetGAI(),
-		Name:                    *api.NewNullableString(common.Ptr(ast.GetName())),
-		AssetType:               ast.GetAssetType(),
-		Description:             *api.NewNullableString(common.Ptr(ast.GetDescription())),
-		ParentFunctionalAssetId: *api.NewNullableInt32(functionalParentAssetId),
-		ParentLocationalAssetId: *api.NewNullableInt32(locationalParentAssetId),
-	}
-	assetID, err := UpsertAsset(a)
-	if err != nil {
-		return nil, created, fmt.Errorf("upserting asset %+v into Eliona: %v", a, err)
-	}
 	if assetID == nil {
-		return nil, created, fmt.Errorf("cannot create asset %s", ast.GetName())
+		a := &AssetToCreate{
+			node:                node,
+			locationalParentGAI: locationalParentGAI,
+			functionalParentGAI: functionalParentGAI,
+		}
+		*assets = append(*assets, a)
 	}
-	created = true
 
-	if err := ast.SetAssetID(*assetID, projectId); err != nil {
-		return nil, created, fmt.Errorf("setting asset id: %v", err)
+	if ln, ok := node.(LocationalNode); ok {
+		for _, child := range ln.GetLocationalChildren() {
+			if child == nil {
+				continue
+			}
+			err := collectAssetsToCreate(child, node.GetGAI(), functionalParentGAI, assets, projectId)
+			if err != nil {
+				return err
+			}
+		}
 	}
-	return assetID, created, nil
+
+	if fn, ok := node.(FunctionalNode); ok {
+		for _, child := range fn.GetFunctionalChildren() {
+			if child == nil {
+				continue
+			}
+			err := collectAssetsToCreate(child, locationalParentGAI, node.GetGAI(), assets, projectId)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
 
-func setAssetParent(assetId *int32, ast Asset, projectId string, locationalParentAssetId *int32, functionalParentAssetId *int32) error {
-	asset := api.Asset{
-		Id:                    *api.NewNullableInt32(assetId),
-		ProjectId:             projectId,
-		GlobalAssetIdentifier: ast.GetGAI(),
-		AssetType:             ast.GetAssetType(),
-	}
-	fetchedAsset, _, err := client.NewClient().AssetsAPI.
-		GetAssetById(client.AuthenticationContext(), asset.GetId()).
-		Execute()
+// collectAssetsAndDataToCreate traverses the asset tree and collects assets that need to be created,
+// as well as data to be upserted.
+func collectAssetsAndDataToCreate(node Asset, locationalParentGAI, functionalParentGAI string, assets *[]AssetWithParentReferences, data *[]Data, projectId string, ts *time.Time, clientReference *string) error {
+	assetID, err := node.GetAssetID(projectId)
 	if err != nil {
-		tools.LogError(fmt.Errorf("getting asset %v: %w", asset.GetId(), err))
-	}
-	if locationalParentAssetId != nil && !fetchedAsset.HasParentLocationalAssetId() {
-		asset.ParentLocationalAssetId = *api.NewNullableInt32(locationalParentAssetId)
-	}
-	if functionalParentAssetId != nil && !fetchedAsset.HasParentFunctionalAssetId() {
-		asset.ParentFunctionalAssetId = *api.NewNullableInt32(functionalParentAssetId)
+		return fmt.Errorf("getting asset ID: %v", err)
 	}
 
-	// TODO: This is an ugly workaround to not overwrite name changes in Eliona.
-	// We have to get rid of this once APIv2 starts skipping empty fields.
-	if !asset.Name.IsSet() {
-		asset.Name = fetchedAsset.Name
+	if assetID == nil {
+		a := &AssetToCreate{
+			node:                node,
+			locationalParentGAI: locationalParentGAI,
+			functionalParentGAI: functionalParentGAI,
+		}
+		*assets = append(*assets, a)
+	} else {
+		// Prepare data to upsert for existing assets
+		dataToUpsert := Data{
+			AssetId:         *assetID,
+			Timestamp:       *api.NewNullableTime(ts),
+			ClientReference: *clientReference,
+			Data:            node,
+		}
+		*data = append(*data, dataToUpsert)
 	}
-	if !asset.Description.IsSet() {
-		asset.Description = fetchedAsset.Description
-	}
-	//
 
-	if _, err := UpsertAsset(asset); err != nil {
-		return fmt.Errorf("upserting asset %+v into Eliona: %v", asset, err)
+	if ln, ok := node.(LocationalNode); ok {
+		for _, child := range ln.GetLocationalChildren() {
+			if child == nil {
+				continue
+			}
+			err := collectAssetsAndDataToCreate(child, node.GetGAI(), functionalParentGAI, assets, data, projectId, ts, clientReference)
+			if err != nil {
+				return err
+			}
+		}
 	}
+
+	if fn, ok := node.(FunctionalNode); ok {
+		for _, child := range fn.GetFunctionalChildren() {
+			if child == nil {
+				continue
+			}
+			err := collectAssetsAndDataToCreate(child, locationalParentGAI, node.GetGAI(), assets, data, projectId, ts, clientReference)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	return nil
+}
+
+// AssetToCreate implements AssetWithParentReferences and holds asset information for creation.
+type AssetToCreate struct {
+	node                Asset
+	locationalParentGAI string
+	functionalParentGAI string
+}
+
+func (a *AssetToCreate) GetName() string {
+	return a.node.GetName()
+}
+
+func (a *AssetToCreate) GetDescription() string {
+	return a.node.GetDescription()
+}
+
+func (a *AssetToCreate) GetAssetType() string {
+	return a.node.GetAssetType()
+}
+
+func (a *AssetToCreate) GetGAI() string {
+	return a.node.GetGAI()
+}
+
+func (a *AssetToCreate) GetLocationalParentGAI() string {
+	return a.locationalParentGAI
+}
+
+func (a *AssetToCreate) GetFunctionalParentGAI() string {
+	return a.functionalParentGAI
+}
+
+func (a *AssetToCreate) SetAssetID(assetID int32, projectID string) error {
+	return a.node.SetAssetID(assetID, projectID)
 }
